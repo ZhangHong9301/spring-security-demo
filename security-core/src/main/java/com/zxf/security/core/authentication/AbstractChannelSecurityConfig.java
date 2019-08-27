3f291b1818d7e98826cd23002b7e39394c0d490e
ZhangHong9301 <1253993436@qq.com>
2019-08-28 00:11:11 +0800

    update

--- security-core/src/main/java/com/zxf/security/core/authentication/AbstractChannelSecurityConfig.java
+++ security-core/src/main/java/com/zxf/security/core/authentication/AbstractChannelSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.zxf.security.core.authentication;
 
 import com.zxf.security.core.properties.SecurityConstants;
+import com.zxf.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,9 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    protected SecurityProperties properties;
+
+    @Autowired
     protected AuthenticationSuccessHandler myAuthenticationSuccessHandler;
 
     @Autowired
@@ -17,7 +21,7 @@ public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter 
 
     protected void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage(SecurityConstants.DEFAULT_LOGIN_PAGE_URL)
+                .loginPage(properties.getBrowser().getLoginPage())
                 .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
                 .successHandler(myAuthenticationSuccessHandler)
                 .failureHandler(myAuthenticationFailureHandler);
